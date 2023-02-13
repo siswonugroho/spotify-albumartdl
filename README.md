91ccf4b81d47e8ef5daa2bf3e715bcc435db0b6b
Sovana Siswonugroho <vana.hantoro7@gmail.com>
2023-02-13 08:45:11 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Spotify Album Art Downloader
+
+Download Spotify album art in 640x640 resolution from a song URL.
+---
+
+You can get the song URL by clicking menu Share > Copy link from the song you want in Spotify app.
+
+This is based on OEmbed URL trick which pretty complicated for some people and the image quality is very low (300x300). So I tried to simplify that process and figured out how to get the higher quality image by modifying the image URL.
---
+++ js/index.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.forms[0];
+  const URLinput = form.elements[0];
+  const alertError = document.querySelector('#alert-failed');
+  const alertLoading = document.querySelector('#alert-loading');
+  const cardAlbum = document.querySelector('#card-img-album');
+  const title = document.querySelector('#song-title');
+  const albumImg = document.querySelector('.img-album');
+  const downloadBtn = document.querySelector('a#download-link');
+
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    // loading state
+    alertError.classList.add('d-none');
+    alertLoading.classList.remove('d-none');
+    fetch(`https://open.spotify.com/oembed?url=${URLinput.value}`)
+    .then(response => {
+      alertLoading.classList.add('d-none');
+      if (!response.ok) {
+        alertError.classList.remove('d-none');
+      }
+      return response.json();
+    })
+    .then(responseJson => {
+      alertError.classList.add('d-none');
+      cardAlbum.classList.remove('d-none');
+      title.textContent = responseJson.title;
+      albumImg.src = responseJson.thumbnail_url;
+      downloadBtn.href = responseJson.thumbnail_url.replace('1e02', 'b273');
+    });
+  })
+});
